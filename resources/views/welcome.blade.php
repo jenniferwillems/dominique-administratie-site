0967c3a639218d1a862de2d2ef75100bb0eedd31
Sam Brands <d286050@edu.rocwb.nl>
2022-11-29 11:46:18 +0100

    Imported Bootstrap and Popper with npm

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,9 @@
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-
+        
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
