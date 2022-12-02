6876bb77d7861a7c9b8e4ff83976299d9e2ed29a
JoeyHuijben <72135632+JoeyHuijben@users.noreply.github.com>
2022-12-02 11:48:34 +0100

    edit pages of books, movies and games are filled with data

--- resources/views/pages/books/edit.blade.php
+++ resources/views/pages/books/edit.blade.php
@@ -1,1 +1,49 @@
-<?php
+@extends('layouts.base')
+
+@section('content')
+    <div class="pt-5">
+        <h1>Boeken</h1>
+        <h2>Boek bewerken</h2>
+
+        <form method="post" action="{{route('books.update', $book)}}">
+            @csrf
+            @method('PUT')
+
+            <div class="form-group mb-3">
+                <label for="title" class="form-label">Titel</label>
+                <input type="text" name="title" class="form-control" id="title" value="{{$book->title}}">
+            </div>
+
+            <div class="form-group mb-3">
+                <label for="series" class="form-label">Reeks</label>
+                <input type="text" name="series" class="form-control" id="series" value="{{$book->series}}">
+            </div>
+
+            <div class="form-group mb-3">
+                <label for="code" class="form-label">Code</label>
+                <input type="text" name="code" class="form-control" value="{{$book->code}}">
+            </div>
+
+            <div class="d-flex p-2 bd-highlight">
+                <form action="POST" action=""{{route('books.update', $book)}}>
+                    @csrf
+                    @method('PUT')
+                    <input type="submit" value="Save Book" class="btn btn-primary">
+                </form>
+
+                <div class="mx-2">
+                    <div class="buttons d-flex">
+                        <form action=""></form>
+                        <form method="POST" action="{{route('books.destroy', $book)}}">
+                            @csrf
+                            @method('delete')
+                            <input class="btn btn-danger" type="submit" value="Delete">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+
+@endsection
+
--- resources/views/pages/movies/edit.blade.php
+++ resources/views/pages/movies/edit.blade.php
@@ -1,1 +1,51 @@
-<?php
+@extends('layouts.base')
+
+@section('content')
+    <div class="pt-5">
+        <h1>Films</h1>
+        <h2>Film bewerken</h2>
+
+        <form method="post" action="{{route('movies.update', $movie)}}">
+            @csrf
+            @method('PUT')
+
+            <div class="form-group mb-3">
+                <label for="name" class="form-label">Naam</label>
+                <input type="text" name="name" class="form-control" id="name" value="{{$movies->name}}">
+            </div>
+
+            <div class="form-group mb-3">
+                <label for="genre" class="form-label">Genre</label>
+                <select class="form-select" id="tag" name="tag">
+                    <option selected disabled>Selecteer een genre</option>
+                    @foreach($tags as $tag)
+                        <option value="{{$tag->id}}">{{$tag->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+
+            <div class="d-flex p-2 bd-highlight">
+                <form action="POST" action=""{{route('movies.update', $movie)}}>
+                    @csrf
+                    @method('PUT')
+                    <input type="submit" value="Save Movie" class="btn btn-primary">
+                </form>
+
+                <div class="mx-2">
+                    <div class="buttons d-flex">
+                        <form action=""></form>
+                        <form method="POST" action="{{route('movies.destroy', $movie)}}">
+                            @csrf
+                            @method('delete')
+                            <input class="btn btn-danger" type="submit" value="Delete">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+
+@endsection
+
+
+
