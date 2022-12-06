dac10fd292b126527b09edb36b15ee307f81cb9d
Sam Brands <d286050@edu.rocwb.nl>
2022-12-06 11:40:30 +0100

    Included livewire styles and scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,14 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-
+        
+        <!-- Styles -->
+        @livewireStyles
+        
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        @livewireScripts
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
