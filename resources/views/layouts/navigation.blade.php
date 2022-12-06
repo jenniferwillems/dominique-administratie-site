a54b0c8385f56fae6807d502c60469a00b8c2d36
Sam Brands <d286050@edu.rocwb.nl>
2022-12-06 10:50:25 +0100

    Fixed broken x-slot
    Used to throw an error because the slot was renamed but not closed properly.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -35,7 +35,7 @@
             @auth
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
-                    <div name="trigger">
+                    <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                                 <div>{{ Auth::user()->name }}</div>
                             <div class="ml-1">
@@ -44,7 +44,6 @@
                                 </svg>
                             </div>
                         </button>
-                    </div>
                     </x-slot>
 
                     <x-slot name="content">
