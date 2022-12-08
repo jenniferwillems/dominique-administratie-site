10f8fb66b59498435ad1712a779bc5d7634168ba
Sam Brands <d286050@edu.rocwb.nl>
2022-12-08 16:15:57 +0100

    Added Select2 for console selection on game create page

--- resources/views/pages/games/create.blade.php
+++ resources/views/pages/games/create.blade.php
@@ -1,27 +1,35 @@
 <x-app-layout>
-    <div class="pt-5 w-25 mx-auto">
-        <h1>Games</h1>
-        <h2>Voeg een game toe</h2>
-        <form method="post" action="{{route('games.store')}}">
-            @csrf
+	<div class="pt-5 w-25 mx-auto">
+		<h1>Games</h1>
+		<h2>Voeg een game toe</h2>
+		<form method="post" action="{{route('games.store')}}">
+			@csrf
 
-            <div class="form-group mb-3">
-                <label for="title" class="form-label">Titel</label>
-                <input type="text" name="title" class="form-control" id="title">
-            </div>
+			<div class="form-group mb-3">
+				<label for="title" class="form-label">Titel</label>
+				<input type="text" name="title" class="form-control" id="title">
+			</div>
 
-            <div class="form-group mb-3">
-                <label for="genre" class="form-label">Genre</label>
-                <select class="form-select" id="tag" name="tag">
-                    <option selected disabled>Selecteer een genre</option>
-                    @foreach($tags as $tag)
-                        <option value="{{$tag->id}}">{{$tag->name}}</option>
-                    @endforeach
-                </select>
-            </div>
+			<div class="form-group mb-3">
+				<label for="consoles" class="form-label">Consoles</label>
+				<select name="consoles[]" id="consoles" class="form-control select-2 w-100" multiple="multiple">
+				</select>
+			</div>
 
-            <input type="submit" value="Save Game" class="btn btn-primary">
+			<input type="submit" value="Save Game" class="btn btn-primary">
 
-        </form>
-    </div>
+		</form>
+	</div>
+	<script type="module">
+		let oldInput = {!! json_encode(old('consoles') ?? []) !!};
+		$( document ).ready( function () {
+			$( ".select-2" ).select2( {
+				tags: true,
+				tokenSeparators: [ ',', ' ' ],
+				theme: 'bootstrap4',
+				data: {!! json_encode($tags) !!}
+			} )
+				.val( oldInput ).trigger( 'change' );
+		} );
+	</script>
 </x-app-layout>
