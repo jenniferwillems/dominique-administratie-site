4964a251b5fe246659c5cf71c7724f1022577ab8
Sam Brands <d286050@edu.rocwb.nl>
2022-12-08 15:38:41 +0100

    Added jQuery and Select2 to window

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -13,6 +13,12 @@ import * as bootstrap from 'bootstrap';
 import axios from 'axios';
 window.axios = axios;
 
+import jQuery from 'jquery';
+window.$ = jQuery;
+
+import select2 from 'select2';
+window.select2 = select2();
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
