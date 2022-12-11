197b15f8774fda526fa454a4cec81ecafed18897
Sam Brands <d286050@edu.rocwb.nl>
2022-12-11 10:00:27 +0100

    Made Tag columns fillable

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -9,6 +9,8 @@ class Tag extends Model
 {
     use HasFactory;
     
+    protected $fillable = ['name', 'category_id'];
+    
     public function games()
     {
         return $this->belongsToMany(Game::class);
