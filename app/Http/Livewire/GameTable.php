abaa74086a1f1eb8818b37a3456b6f13757db631
Sam Brands <d286050@edu.rocwb.nl>
2022-12-12 12:59:24 +0100

    Added tag filtering to games

--- app/Http/Livewire/GameTable.php
+++ app/Http/Livewire/GameTable.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Game;
+use App\Models\Tag;
 use Livewire\WithPagination;
 use Livewire\Component;
 
@@ -11,16 +12,33 @@ class GameTable extends Component
     use WithPagination;
 
     public $search = '';
+    public $selectedTag;
 
     public function updatingSearch()
     {
         $this->resetPage();
     }
 
+    public function updatedSelectedTag()
+    {
+        dd('updated');
+        $this->resetPage();
+    }
+
+    public function addFilter($tag = null)
+    {
+        $this->selectedTag = $tag;
+    }
+
     public function render()
     {
         return view('livewire.game-table', [
-            'games' => Game::where('title', 'like', '%' . $this->search . '%')->paginate(20),
+            'games' => Game::where('title', 'like', '%' . $this->search . '%')
+                ->when($this->selectedTag, function ($query) {
+                    return $query->whereHas('consoles', function ($subquery) {
+                        $subquery->where('name', $this->selectedTag['name']);
+                    });
+                })->paginate(20),
         ]);
     }
 }
--- resources/views/livewire/game-table.blade.php
+++ resources/views/livewire/game-table.blade.php
@@ -2,12 +2,23 @@
 	<x-flash-message/>
 	<div class="d-flex justify-content-between align-items-center">
 		<h1>Games</h1>
-		<div class="w-50 ps-5 d-flex justify-content-between">
-			<input type="text" class="form-control w-50 ms-5" placeholder="Zoeken..." wire:model="search">
+		<div class="mw-75 d-flex justify-content-between align-items-center">
+			<div class="flex-grow-1">
+				@if($selectedTag)
+					Filter:
+					<x-badge class="bg-white" type="dismissible">{{$selectedTag['name']}}
+					</x-badge>
+				@endif
+			</div>
 
-            @if(Auth::user())
-			    <x-buttons.primary href="{{route('games.create')}}">Nieuwe game</x-buttons.primary>
-            @endif
+			<div class="mw-50 flex-shrink-0">
+				<input type="text" class="form-control w-75 ms-5" placeholder="Zoeken..." wire:model="search">
+			</div>
+
+			@if(Auth::user())
+				<x-buttons.primary href="{{route('games.create')}}" class="flex-shrink-0">Nieuwe game
+				</x-buttons.primary>
+			@endauth
 		</div>
 	</div>
 
@@ -16,7 +27,7 @@
 			<tr>
 				<th>ID</th>
 				<th>Titel</th>
-				<th>Console</th>
+				<th>Consoles</th>
 				<th></th>
 			</tr>
 		</thead>
@@ -27,17 +38,19 @@
 					<td>{{$game->title}}</td>
 					<td>
 						@foreach($game->consoles as $console)
-							<x-badge>{{$console->name}}</x-badge>
+							<x-badge wire:click="addFilter({{$console}})"
+							         class="bg-white">{{$console->name ?? ''}}</x-badge>
 						@endforeach
+
 					</td>
 
-                    @if(Auth::user())
-					<td>
-						<x-buttons.outline.dark href="{{route('games.edit', $game)}}" class="btn-sm">
-							<i class="bi bi-pencil"></i>
-						</x-buttons.outline.dark>
-					</td>
-                    @endif
+					@if(Auth::user())
+						<td>
+							<x-buttons.outline.dark href="{{route('games.edit', $game)}}" class="btn-sm">
+								<i class="bi bi-pencil"></i>
+							</x-buttons.outline.dark>
+						</td>
+					@endif
 				</tr>
 			@endforeach
 		</tbody>
