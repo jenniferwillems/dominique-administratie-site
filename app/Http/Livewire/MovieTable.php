9aac07b48d13847d79c105047968a2c0dbdeff14
Sam Brands <d286050@edu.rocwb.nl>
2022-12-12 13:06:20 +0100

    Added tag filtering to movies

--- app/Http/Livewire/MovieTable.php
+++ app/Http/Livewire/MovieTable.php
@@ -11,16 +11,28 @@ class MovieTable extends Component
     use WithPagination;
 
     public $search = '';
+    public $selectedTag;
+
 
     public function updatingSearch()
     {
         $this->resetPage();
     }
 
+    public function addFilter($tag = null)
+    {
+        $this->selectedTag = $tag;
+    }
+
     public function render()
     {
         return view('livewire.movie-table', [
-            'movies' => Movie::where('title', 'like', '%' . $this->search . '%')->paginate(20),
+            'movies' => Movie::where('title', 'like', '%' . $this->search . '%')
+                ->when($this->selectedTag, function ($query) {
+                    return $query->whereHas('genres', function ($subquery) {
+                        $subquery->where('name', $this->selectedTag['name']);
+                    });
+                })->paginate(20),
         ]);
     }
 }
--- resources/views/livewire/movie-table.blade.php
+++ resources/views/livewire/movie-table.blade.php
@@ -2,12 +2,23 @@
 	<x-flash-message/>
 	<div class="d-flex justify-content-between align-items-center">
 		<h1>Films</h1>
-		<div class="w-50 ps-5 d-flex justify-content-between">
-			<input type="text" class="form-control w-50 ms-5" placeholder="Zoeken..." wire:model="search">
+		<div class="mw-75 d-flex justify-content-between align-items-center">
+			<div class="flex-grow-1">
+				@if($selectedTag)
+					Filter:
+					<x-badge class="bg-white" type="dismissible">{{$selectedTag['name']}}
+					</x-badge>
+				@endif
+			</div>
 
-            @if(Auth::user())
-			    <x-buttons.primary href="{{route('movies.create')}}">Nieuwe film</x-buttons.primary>
-            @endif
+			<div class="mw-50 flex-shrink-0">
+				<input type="text" class="form-control w-75 ms-5" placeholder="Zoeken..." wire:model="search">
+			</div>
+
+			@if(Auth::user())
+				<x-buttons.primary href="{{route('games.create')}}" class="flex-shrink-0">Nieuwe film
+				</x-buttons.primary>
+			@endauth
 		</div>
 	</div>
 
@@ -16,7 +27,7 @@
 			<tr>
 				<th>ID</th>
 				<th>Titel</th>
-				<th>Genre</th>
+				<th>Genres</th>
 				<th></th>
 			</tr>
 		</thead>
@@ -27,17 +38,19 @@
 					<td>{{$movie->title}}</td>
 					<td>
 						@foreach($movie->genres as $genre)
-							<x-badge>{{$genre->name}}</x-badge>
+							<x-badge wire:click="addFilter({{$genre}})"
+							         class="bg-white">{{$genre->name ?? ''}}</x-badge>
 						@endforeach
+
 					</td>
 
-                    @if(Auth::user())
-					<td>
-						<x-buttons.outline.dark href="{{route('movies.edit', $movie)}}" class="btn-sm">
-							<i class="bi bi-pencil"></i>
-						</x-buttons.outline.dark>
-					</td>
-                    @endif
+					@if(Auth::user())
+						<td>
+							<x-buttons.outline.dark href="{{route('movies.edit', $movie)}}" class="btn-sm">
+								<i class="bi bi-pencil"></i>
+							</x-buttons.outline.dark>
+						</td>
+					@endif
 				</tr>
 			@endforeach
 		</tbody>
