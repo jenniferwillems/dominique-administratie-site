da1c2e160233b89f369f676af057cf90fb29551d
Sam Brands <d286050@edu.rocwb.nl>
2022-12-12 11:35:26 +0100

    Long filter tags no longer wrap

--- resources/views/components/buttons/primary.blade.php
+++ resources/views/components/buttons/primary.blade.php
@@ -1,3 +1,3 @@
-<div>
+<div class="mw-50 flex-shrink-0">
     <a href="{{$href}}" class="btn btn-primary" role="button">{{$slot}}</a>
 </div>
--- resources/views/livewire/book-table.blade.php
+++ resources/views/livewire/book-table.blade.php
@@ -2,20 +2,20 @@
 	<x-flash-message/>
 	<div class="d-flex justify-content-between align-items-center">
 		<h1>Boeken</h1>
-		<div class="w-50 d-flex justify-content-between align-items-center">
-			<div class="w-25">
+		<div class="mw-75 d-flex justify-content-between align-items-center">
+			<div class="flex-grow-1">
 				@if($selectedTag)
 					Filter: <x-badge class="bg-white" type="dismissible">{{$selectedTag['name']}}
 					</x-badge>
 				@endif
 			</div>
 
-			<div class="w-50">
+			<div class="mw-50 flex-shrink-0">
 				<input type="text" class="form-control w-75 ms-5" placeholder="Zoeken..." wire:model="search">
 			</div>
 
             @if(Auth::user())
-                <x-buttons.primary href="{{route('books.create')}}">Nieuw boek</x-buttons.primary>
+                <x-buttons.primary href="{{route('books.create')}}" class="flex-shrink-0">Nieuw boek</x-buttons.primary>
             @endauth
 		</div>
 	</div>
