1aff7bfb671baef685c0932f6210cfa1a3405893
Sam Brands <d286050@edu.rocwb.nl>
2022-12-12 11:15:40 +0100

    Added tag filtering to books

--- app/Http/Livewire/BookTable.php
+++ app/Http/Livewire/BookTable.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Book;
+use App\Models\Tag;
 use Livewire\WithPagination;
 use Livewire\Component;
 
@@ -11,16 +12,25 @@ class BookTable extends Component
     use WithPagination;
 
     public $search = '';
+    public $selectedTag = [];
 
     public function updatingSearch()
     {
         $this->resetPage();
     }
     
+    public function addFilter($tag = null)
+    {
+        $this->selectedTag = $tag;
+    }
+
     public function render()
     {
         return view('livewire.book-table', [
-            'books' => Book::where('title', 'like', '%' . $this->search . '%')->paginate(20),
+            'books' => Book::where('title', 'like', '%' . $this->search . '%')
+                ->when($this->selectedTag, function($query) {
+                    return $query->where('series_id', $this->selectedTag['id']);
+                })->paginate(20),
         ]);
     }
 }
--- resources/views/livewire/book-table.blade.php
+++ resources/views/livewire/book-table.blade.php
@@ -2,15 +2,24 @@
 	<x-flash-message/>
 	<div class="d-flex justify-content-between align-items-center">
 		<h1>Boeken</h1>
-		<div class="w-50 ps-5 d-flex justify-content-between">
-			<input type="text" class="form-control w-50 ms-5" placeholder="Zoeken..." wire:model="search">
+		<div class="w-50 d-flex justify-content-between align-items-center">
+			<div class="w-25">
+				@if($selectedTag)
+					Filter: <x-badge class="bg-white" type="dismissible">{{$selectedTag['name']}}
+					</x-badge>
+				@endif
+			</div>
+
+			<div class="w-50">
+				<input type="text" class="form-control w-75 ms-5" placeholder="Zoeken..." wire:model="search">
+			</div>
 
             @if(Auth::user())
                 <x-buttons.primary href="{{route('books.create')}}">Nieuw boek</x-buttons.primary>
             @endauth
 		</div>
 	</div>
-
+	
 	<table class="table table-striped table-hover">
 		<thead>
 			<tr>
@@ -27,7 +36,7 @@
 					<td>{{$book->id}}</td>
 					<td>{{$book->title}}</td>
 					<td>
-						<x-badge>{{$book->series->name ?? ''}}</x-badge>
+						<x-badge wire:click="addFilter({{$book->series}})" class="bg-white">{{$book->series->name ?? ''}}</x-badge>
 					</td>
 					<td>{{$book->code}}</td>
 
