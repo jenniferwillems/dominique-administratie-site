b5c70ca975f76d36d1eec3b07baff557aaac7540
Sam Brands <d286050@edu.rocwb.nl>
2022-12-13 11:06:59 +0100

    Added tag category methods for formatting

--- app/Models/TagCategory.php
+++ app/Models/TagCategory.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class TagCategory extends Model
 {
     use HasFactory;
+
+    public function scopeAllCategories($query, $format = false)
+    {
+        $categories = TagCategory::all();
+        if ($format) {
+            return $this->formatCategories($categories);
+        }
+        return $categories;
+    }
+    
+    public function formatCategories($categories)
+    {
+        return $categories->map(function ($category) {
+            return ['id' => $category->id, 'text' => $category->name];
+        })->toArray();
+    }
 }
